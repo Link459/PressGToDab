81e8bb08fb4dc7700d1c231b8b7d892839a574ce
Link459 <97700587+Link459@users.noreply.github.com>
2022-04-20 20:39:10 +0200

    Add project files.

---
+++ PressGToDab/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("PressGToDab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PressGToDab")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e43829dd-6a0f-4f0a-b3b4-68d8229b2950")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
